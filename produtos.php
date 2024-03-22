ad345d994b28ba629cb22e4bcbc151eece71032f
BreninSenna <127742561+BreninSenna@users.noreply.github.com>
2024-03-22 12:25:49 -0300

    Add files via upload

---
+++ produtos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculos dos Produtos</title>
+</head>
+<body>
+    <?php
+    echo "<h3 align='center'>Cálculo e Exibição dos produtos</h3>";
+    echo "<hr size='3' color='red'>";
+    
+    echo "Código:" .$var1;
+    ?>
+</body>
+</html>
